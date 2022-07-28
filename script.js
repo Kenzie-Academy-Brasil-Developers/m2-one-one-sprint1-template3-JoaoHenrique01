221c1ac1b628442c9955449c92dd6d57561c6b94
unknown <joao_henrique_g@outlook.com>
2022-07-28 12:07:18 -0300

    Terminei

--- script.js
+++ script.js
@@ -31,3 +31,29 @@ const listaDeDevs = [
   },
 ];
 
+
+//Função para renderizar os cards;
+function renderizarCards(array) {
+  //Capturar a section e o footer
+    const section = document.getElementById("devs__list");
+    const footer = document.getElementById("main__footer");
+
+
+    for(let i=0; i<array.length; i++) {
+      const div = document.createElement("div");
+      const h2 = document.createElement("h2");
+      const img = document.createElement("img"); //Irá receber um valor em src;
+
+      //Adicionar dados aos elementos criados.
+      h2.innerText(array[i].nome);
+      img.src.innerText(array[i].imagem);
+
+      
+
+      div.append(h2, img);
+      section.appendChild(div);
+
+
+    }
+
+}
